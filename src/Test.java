37634319b4b9dfd5af516f6013780096c245e155
mihira jena <mihira jena@172.30.34.88>
2015-04-22 13:37:05 +0530

    update code

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("hi welcome to my git project");
+		System.out.println("hi welcome to my git project online");
 
 	}
 
